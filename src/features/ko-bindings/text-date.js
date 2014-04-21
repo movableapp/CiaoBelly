d6c12471f843adaef167cb2a10bff622b89ca782
thepeg <marco.pegoraro@gmail.com>
2014-04-21 20:08:56 +0200

    basic display

--- src/features/ko-bindings/text-date.js
+++ src/features/ko-bindings/text-date.js
@@ -13,3 +13,18 @@ window.ko.bindingHandlers.textDate = {
         element.innerHTML = strDate;
     }
 };
+
+window.ko.bindingHandlers.textTimestamp = {
+    update: function(element, valueAccessor) {
+        var timestamp = ko.unwrap(valueAccessor());
+        var date = new Date(timestamp);
+        
+        var strDate = date.toString();
+        
+        strDate = strDate.substr(strDate.indexOf(" "), 255);
+        strDate = strDate.substr(0, strDate.indexOf(date.getFullYear()));
+        strDate+= '<small>' + date.getFullYear() + '</small>';
+        
+        element.innerHTML = strDate;
+    }
+};
